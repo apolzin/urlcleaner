6f98327d942d170921b5495f5f716775e009ab48
apolzin <alexander@girls.io>
2011-06-23 23:31:44 +0200

    initial

---
+++ src/apolzin/unshitify/Unshitify.scala
@@ -0,0 +1,88 @@
+package apolzin.unshitify
+
+import javax.servlet.http.{HttpServletResponse, HttpServletRequest, HttpServlet}
+import vo.DomainKey
+import util.control.Breaks._
+import java.net.URLEncoder
+import java.io.BufferedReader
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: alexander
+ * Date: 6/23/11
+ * Time: 10:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Unshitify extends HttpServlet {
+    override def doGet(req: HttpServletRequest, resp: HttpServletResponse) {
+
+    resp.setContentType("text/plain")
+
+    if(req.getParameter("url") != null){
+      val pm = PMF.get.getPersistenceManager
+
+      var decodedURL = req.getQueryString.replace("url=","")
+
+      var domainKey = new DomainKey(decodedURL)
+      val key = domainKey.createKey
+
+      val originalHeader = header(escapeURL(decodedURL))
+
+      try{
+         domainKey = pm.getObjectById(classOf[DomainKey], key)
+      } catch {
+        case e => ""
+      }
+
+      breakable {
+        while(decodedURL.matches(".*[?&].*")){
+          val opts = extractLastQueryPair(decodedURL)
+          decodedURL = decodedURL.replaceFirst("[&?][^&^?^=]+=[^&^?^=]+$","")
+          if(!domainKey.hasBannedKey(opts(0))){
+            var testHeader = header(escapeURL(decodedURL))
+            if(testHeader == originalHeader){
+              domainKey.addToBannedKeys(opts(0))
+            } else {
+              break()
+            }
+          }
+        }
+      }
+      pm.makePersistent(domainKey)
+      pm.close()
+      resp.getWriter.println(decodedURL)
+    }
+    else {
+      resp.getWriter.println("no url supplied")
+    }
+
+    def extractLastQueryPair(url: String) = {
+      val reg = ".*[&?]([^&^?^=]+=[^&^?^=]+)$".r
+      val reg(opts) = url
+      opts.split("=")
+    }
+
+    def escapeURL(url: String) = {
+      val reg = "[&?][^&^?^=]+=[^&^?^=]+".r
+      reg.replaceAllIn(url, m => m.matched.split("=")(0) + "=" + URLEncoder.encode(m.matched.split("=")(1), "utf-8"))
+    }
+
+    def header(url: String) = {
+      val source = io.Source.fromURL(url, "utf-8").reader()
+      val buff = new BufferedReader(source)
+      var ret = ""
+      var out = ""
+      breakable{
+        while({out = buff.readLine(); out != null}) {
+          ret += out
+          if(out.matches("<body")){
+            break()
+          }
+        }
+      }
+      ret
+    }
+  }
+
+}
---
+++ src/apolzin/unshitify/vo/DomainKey.java
@@ -0,0 +1,86 @@
+package apolzin.unshitify.vo;
+
+
+
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+
+
+
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+import java.util.ArrayList;
+
+
+@PersistenceCapable
+public class DomainKey {
+
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Key key;
+	@Persistent
+	private ArrayList<String> bannedKeys;
+    @Persistent
+    private String domain;
+
+	public DomainKey(String url)
+	{
+        domain = normalize(url);
+        key = createKey();
+
+	}
+
+
+    private String normalize(String url)
+    {
+        return(url.replaceFirst("^(http:\\/\\/)?\\.?(www)?\\.?","").
+            replaceFirst("\\/.*$", ""));
+    }
+
+    public String getDomain()
+    {
+        return domain;
+    }
+
+    public ArrayList<String> getBannedKeys()
+    {
+        return bannedKeys;
+    }
+
+    public void addToBannedKeys(String key)
+    {
+        try{
+            bannedKeys.add(key);
+        }
+        catch(Exception e)
+        {
+
+        }
+    }
+
+    public boolean hasBannedKey(String key)
+    {
+        try
+        {
+            return bannedKeys.contains(key);
+        }
+        catch(Exception e)
+        {
+            return false;
+        }
+
+    }
+
+    public void setBannedKeys(ArrayList<String> keys)
+    {
+        this.bannedKeys = keys;
+    }
+
+	public Key createKey()
+	{
+        return KeyFactory.createKey(DomainKey.class.getSimpleName(), normalize(this.domain));
+	}
+}
