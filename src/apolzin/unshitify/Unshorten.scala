fcfbd9847822f8dc1aefd6a4ccf46bd06e22aaf8
apolzin <alexander@girls.io>
2011-07-12 00:37:49 +0200

    meh

--- src/apolzin/unshitify/Unshorten.scala
+++ src/apolzin/unshitify/Unshorten.scala
@@ -3,6 +3,9 @@ package apolzin.unshitify
 import javax.servlet.http.{HttpServletResponse, HttpServletRequest, HttpServlet}
 
 import java.net.{URLEncoder, HttpURLConnection, URL}
+import util.control.Breaks._
+import com.sun.org.apache.xpath.internal.operations.Variable
+import org.datanucleus.store.mapped.expression.ExpressionLogicSetAdapter
 
 class Unshorten extends HttpServlet{
   override def doGet(req: HttpServletRequest, resp: HttpServletResponse) {
@@ -15,6 +18,7 @@ class Unshorten extends HttpServlet{
             if(tco_head.matches("^http.*")){
               resp.getWriter.println(tco_head)
             } else {
+
               resp.getWriter.println(head)
             }
            } else {
@@ -39,7 +43,26 @@ class Unshorten extends HttpServlet{
       con.setDoOutput(true)
       con.setInstanceFollowRedirects(false)
       con.connect()
-      con.getHeaderField("Location")
+      //con.getHeaderField("Location")
+      var i = 0
+      var continue = true
+      var location = ""
+      while(continue){
+        if(con.getHeaderFieldKey(i) == null && con.getHeaderField(i) == null ){
+          continue = false
+        } else {
+          if(con.getHeaderFieldKey(i) == "location"){
+            location += con.getHeaderField(i)
+
+          }
+          i += 1
+        }
+      }
+      if(location == ""){
+        null
+      } else {
+        location
+      }
     } catch {
       case e => url
     }
